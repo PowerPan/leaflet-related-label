42d56ca21480013c50d1826ef9984123257dd2e6
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-11-18 12:02:55 +0100

    add src

---
+++ src/copyright.js
@@ -0,0 +1,9 @@
+/*
+	leaflet-tracksymbol-label, a plugin that adds labels leaflet-trackmarkers for Leaflet powered maps. Based on Leaflet.label
+	(c) 2016, Johannes Rudolph
+
+ 	https://github.com/PowerPan/leaflet-tracksymbol-label
+	https://github.com/Leaflet/Leaflet.label
+	http://leafletjs.com
+	https://github.com/PowerPan
+*/
